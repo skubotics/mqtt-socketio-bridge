7cfa75ccc9f068723075e9bdc87d5cd5aad47ed7
Tunir Saha <sahatunir@gmail.com>
2024-07-03 01:12:08 +0530

    fixes

--- index.js
+++ index.js
@@ -1,70 +1,144 @@
-var mqtt = require('mqtt');
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var cors = require('cors');
+const mqtt = require('mqtt');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-// Port that the web server should listen on
-var port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const moment = require('moment-timezone');
 
-// MQTT Broker URL
-var MQTT_BROKER = process.env.MQTT_BROKER || "mqtt://13.235.151.163";
+const port = process.env.PORT || 3000;
+const MQTT_BROKER = process.env.MQTT_BROKER || "mqtt://13.235.151.163";
+const MQTT_TOPIC = process.env.MQTT_TOPIC || "test";
+const uri = "mongodb+srv://streetjerk:iOmabKFeDS11jYqs@rail.qgm5ln9.mongodb.net/?retryWrites=true&w=majority&appName=rail";
 
-// MQTT Topic to listen to
-var MQTT_TOPIC = process.env.MQTT_TOPIC || "test";
-
-// MQTT Client
-var client = mqtt.connect(MQTT_BROKER);
-client.on('connect', function () {
-    console.log("Connected to " + MQTT_BROKER);
-    client.subscribe(MQTT_TOPIC, function (err) {
-        if (!err) {
-            console.log("Subscribed to topic: " + MQTT_TOPIC);
-        } else {
-            console.error("Failed to subscribe to topic: " + MQTT_TOPIC, err);
-        }
-    });
+const mongoClient = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
-client.on('message', function (topic, payload) {
-    if (topic === MQTT_TOPIC) {
-        console.log("Received message on topic: " + topic);
-        console.log("Message payload: " + payload.toString());
+async function connectMongoDB() {
+  try {
+    await mongoClient.connect();
+    console.log("Connected to MongoDB!");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+  }
+}
+connectMongoDB();
 
-        const incomingData = payload.toString();
-        const lastHyphenIndex = incomingData.lastIndexOf('-');
-        const base = incomingData.substring(0, lastHyphenIndex);
-        const values = incomingData.substring(lastHyphenIndex + 1);
-        const valuesList = values.split(',');
-        const result = valuesList.map((value, index) => `${base}-CH${index + 1}-${value}`);
-
-        io.emit('mqtt', { 'topic': topic, 'payload': payload.toString(), 'data': JSON.stringify(result) });
+const mqttClient = mqtt.connect(MQTT_BROKER);
+mqttClient.on('connect', function () {
+  console.log("Connected to " + MQTT_BROKER);
+  mqttClient.subscribe(MQTT_TOPIC, function (err) {
+    if (!err) {
+      console.log("Subscribed to topic: " + MQTT_TOPIC);
+    } else {
+      console.error("Failed to subscribe to topic: " + MQTT_TOPIC, err);
     }
+  });
 });
 
-// Socket.IO Handling
+mqttClient.on('message', async function (topic, payload) {
+  if (topic === MQTT_TOPIC) {
+
+    incomingData = payload.toString();
+
+    let pattern = /^CPU1#ADC1#(-?\d+(\.\d+)?,)*(-?\d+(\.\d+)?)$/;
+
+    if (pattern.test(incomingData)) {
+      const [cpu, adc, values] = incomingData.split('#', 3);
+      const valuesList = values.split(',');
+      const result = []
+      const documents = []
+
+      valuesList.forEach((value, index) => {
+        result.push(`${cpu}#${adc}#CH${index + 1}#${value}`)
+        documents.push({
+          device: `${cpu}#${adc}#CH${index + 1}`,
+          value: value,
+          time: moment().tz("Asia/Kolkata").format()
+        })
+      });
+
+      io.emit('mqtt', { 'topic': topic, 'payload': incomingData, 'data': JSON.stringify(result) });
+
+      const db = mongoClient.db("test");
+      const collection = db.collection("test");
+
+      try {
+        await collection.insertMany(documents);
+        console.log("Data inserted into MongoDB");
+      } catch (err) {
+        console.error("Failed to insert data into MongoDB", err);
+      }
+    } else {
+      io.emit('mqtt', { 'topic': topic, 'payload': incomingData });
+    }
+
+  }
+});
+
 io.on('connection', function (sock) {
-    console.log("New connection from " + sock.id);
+  console.log("New connection from " + sock.id);
 
-    sock.on('disconnect', function () {
-        console.log("Socket disconnected: " + sock.id);
-    });
+  sock.on('disconnect', function () {
+    console.log("Socket disconnected: " + sock.id);
+  });
 });
 
-// Static file handling
 app.use(express.static('static_files'));
 
-// Use Cors
 app.use(cors({
-    origin: '*'
+  origin: '*'
 }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/static_files/mqtt-watcher.html");
+  res.sendFile(__dirname + "/static_files/mqtt-watcher.html");
 });
 
-// Start server
+app.delete('/cleardb', async (req, res) => {
+  try {
+    const db = mongoClient.db("test");
+    const collection = db.collection("test");
+    const result = await collection.deleteMany({});
+    console.log(`${result.deletedCount} documents were deleted`);
+    res.send(`${result.deletedCount} documents were deleted`);
+  } catch (err) {
+    console.error("Error deleting documents:", err);
+    res.status(500).send("Failed to delete documents");
+  }
+});
+
+app.post('/history', async (req, res) => {
+  const deviceIds = req.body.deviceIds;
+
+  if (!deviceIds || deviceIds.length === 0) {
+    return res.status(400).send("No device IDs provided.");
+  }
+
+  try {
+    const db = mongoClient.db("test");
+    const collection = db.collection("test");
+
+    const records = await collection.find({ device: { $in: deviceIds } })
+      .sort({ time: 1 })
+      .limit(30)
+      .toArray();
+
+    res.json(records);
+  } catch (err) {
+    console.error("Error fetching data from MongoDB:", err);
+    res.status(500).send("Failed to retrieve data");
+  }
+});
+
 http.listen(port, function () {
-    console.log("Server listening on port " + port);
+  console.log("Server listening on port " + port);
 });
