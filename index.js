3969d2aa84ed4a47b785107d07f88a53cf5850b5
Tunir Saha <sahatunir@gmail.com>
2024-07-27 16:17:23 +0530

    regex update for incoming message

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ mqttClient.on('message', async function (topic, payload) {
   if (topic === MQTT_TOPIC) {
 
     incomingData = payload.toString();
-    let pattern = /^CPU\d+#ADC\d+#(-?\d+(\.\d+)?,)*(-?\d+(\.\d+)?)#(\d{4}-\d{2}-\d{2})#(\d{2}:\d{2}:\d{2})$/;
+    let pattern = /^CPU\d+#ADC\d+#(((-?\d+(\.\d+)?|X),)*(-?\d+(\.\d+)?|X))#(\d{4}-\d{2}-\d{2})#(\d{2}:\d{2}:\d{2})$/;
 
     if (pattern.test(incomingData)) {
       const [cpu, adc, values, date, time] = incomingData.split('#');
